8fe2937b533fdb164d7a1f7a4ce7f4b6bd3ba6a9
David Sim <wsim903@gmail.com>
2018-08-08 01:07:31 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Susan"', () => {
+    it('returns "Joe"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
